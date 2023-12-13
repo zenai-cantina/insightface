2c8195c2869fac9431db5733443f883f08c145e9
zenai-cantina <duong.na@zenai.vn>
2023-12-13 12:35:54 +0700

    inswapper parsing

---
+++ insightface/model_zoo/inswapper.py
@@ -0,0 +1,166 @@
+import time
+import numpy as np
+import onnxruntime
+import cv2
+import onnx
+import facer
+import torch
+from onnx import numpy_helper
+from ..utils import face_align
+
+
+def hwc2bchw(images: torch.Tensor) -> torch.Tensor:
+    return images.unsqueeze(0).permute(0, 3, 1, 2)
+
+
+def to_tensor(images):
+    return hwc2bchw(torch.from_numpy(images))
+
+def create_mask(vis_img):
+    threshold = 0.  # You can adjust this threshold as needed
+    binary_mask = vis_img > threshold
+    
+    # Convert the binary mask to a white object on a black background
+    object_color = 255  # White
+    background_color = 0  # Black
+    
+    white_mask = binary_mask * object_color
+    black_mask = ~binary_mask * background_color
+    
+    # Combine the white object and black background masks
+    result_mask = white_mask + black_mask
+    return result_mask
+
+class INSwapper():
+    def __init__(self, model_file=None, session=None):
+        self.model_file = model_file
+        self.session = session
+        model = onnx.load(self.model_file)
+        graph = model.graph
+        self.emap = numpy_helper.to_array(graph.initializer[-1])
+        self.input_mean = 0.0
+        self.input_std = 255.0
+        #print('input mean and std:', model_file, self.input_mean, self.input_std)
+        if self.session is None:
+            self.session = onnxruntime.InferenceSession(self.model_file, None)
+        inputs = self.session.get_inputs()
+        self.input_names = []
+        for inp in inputs:
+            self.input_names.append(inp.name)
+        outputs = self.session.get_outputs()
+        output_names = []
+        for out in outputs:
+            output_names.append(out.name)
+        self.output_names = output_names
+        assert len(self.output_names)==1
+        output_shape = outputs[0].shape
+        input_cfg = inputs[0]
+        input_shape = input_cfg.shape
+        self.input_shape = input_shape
+        print('inswapper-shape:', self.input_shape)
+        self.input_size = tuple(input_shape[2:4][::-1])
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.face_parser = facer.face_parser('farl/lapa/448', device=self.device)
+
+    def forward(self, img, latent):
+        img = (img - self.input_mean) / self.input_std
+        pred = self.session.run(self.output_names, {self.input_names[0]: img, self.input_names[1]: latent})[0]
+        return pred
+
+    def get_face_parsing_mask(self, face_image, yv5_faces):
+        with torch.inference_mode():
+            faces = self.face_parser(face_image, yv5_faces)
+
+        seg_logits = faces['seg']['logits']
+        seg_probs = seg_logits.softmax(dim=1)  # nfaces x nclasses x h x w
+        n_classes = seg_probs.size(1)
+        vis_seg_probs = seg_probs.argmax(dim=1).float() / n_classes * 255
+        vis_img = vis_seg_probs.sum(0, keepdim=True)
+
+        result_mask = create_mask(vis_img)
+        result_mask = torch.Tensor(result_mask)
+
+        mask_array = result_mask.squeeze().cpu().numpy()
+        # image_seg_array = np.uint8(np.stack([mask_array] * 3, axis=-1))
+        image_seg_array = np.uint8(mask_array[..., None])
+
+        return image_seg_array        
+
+    def get(self, img, target_face, source_face, paste_back=True):
+        aimg, M = face_align.norm_crop2(img, target_face.kps, self.input_size[0])
+        blob = cv2.dnn.blobFromImage(aimg, 1.0 / self.input_std, self.input_size,
+                                      (self.input_mean, self.input_mean, self.input_mean), swapRB=True)
+        latent = source_face.normed_embedding.reshape((1,-1))
+        latent = np.dot(latent, self.emap)
+        latent /= np.linalg.norm(latent)
+        pred = self.session.run(self.output_names, {self.input_names[0]: blob, self.input_names[1]: latent})[0]
+        #print(latent.shape, latent.dtype, pred.shape)
+        img_fake = pred.transpose((0,2,3,1))[0]
+        bgr_fake = np.clip(255 * img_fake, 0, 255).astype(np.uint8)[:,:,::-1]
+        if not paste_back:
+            return bgr_fake, M
+        else:
+            target_img = img
+            fake_diff = bgr_fake.astype(np.float32) - aimg.astype(np.float32)
+            fake_diff = np.abs(fake_diff).mean(axis=2)
+            fake_diff[:2,:] = 0
+            fake_diff[-2:,:] = 0
+            fake_diff[:,:2] = 0
+            fake_diff[:,-2:] = 0
+            IM = cv2.invertAffineTransform(M)
+            img_white = np.full((aimg.shape[0],aimg.shape[1]), 255, dtype=np.float32)
+            bgr_fake = cv2.warpAffine(bgr_fake, IM, (target_img.shape[1], target_img.shape[0]), borderValue=0.0)
+            img_white = cv2.warpAffine(img_white, IM, (target_img.shape[1], target_img.shape[0]), borderValue=0.0)
+            fake_diff = cv2.warpAffine(fake_diff, IM, (target_img.shape[1], target_img.shape[0]), borderValue=0.0)
+            img_white[img_white>20] = 255
+            fthresh = 10
+            fake_diff[fake_diff<fthresh] = 0
+            fake_diff[fake_diff>=fthresh] = 255
+            img_mask = img_white
+            mask_h_inds, mask_w_inds = np.where(img_mask==255)
+            mask_h = np.max(mask_h_inds) - np.min(mask_h_inds)
+            mask_w = np.max(mask_w_inds) - np.min(mask_w_inds)
+            mask_size = int(np.sqrt(mask_h*mask_w))
+            k = max(mask_size//10, 10)
+            #k = max(mask_size//20, 6)
+            #k = 6
+            kernel = np.ones((k,k),np.uint8)
+            img_mask = cv2.erode(img_mask,kernel,iterations = 1)
+            kernel = np.ones((2,2),np.uint8)
+            fake_diff = cv2.dilate(fake_diff,kernel,iterations = 1)
+            k = max(mask_size//20, 5)
+            #k = 3
+            #k = 3
+            kernel_size = (k, k)
+            blur_size = tuple(2*i+1 for i in kernel_size)
+            img_mask = cv2.GaussianBlur(img_mask, blur_size, 0)
+            k = 5
+            kernel_size = (k, k)
+            blur_size = tuple(2*i+1 for i in kernel_size)
+            fake_diff = cv2.GaussianBlur(fake_diff, blur_size, 0)
+            img_mask /= 255
+            fake_diff /= 255
+            #img_mask = fake_diff
+            img_mask = np.reshape(img_mask, [img_mask.shape[0],img_mask.shape[1],1])
+
+            face_image = to_tensor(cv2.cvtColor(bgr_fake, cv2.COLOR_BGR2RGB)).to(device=self.device)
+            # Convert the data to PyTorch tensors
+            boxes_tensor = torch.tensor(np.array([target_face.bbox]), dtype=torch.float32).to(device=self.device)
+            key_points_tensor = torch.tensor(np.array([target_face.kps]), dtype=torch.float32).to(device=self.device)
+
+            # Create the dictionary with the desired format
+            yv5_faces = {
+                'rects': boxes_tensor,
+                'points': key_points_tensor,
+                'scores': torch.tensor([0.99], dtype=torch.float32).to(device=self.device),
+                'image_ids': torch.tensor([0], dtype=torch.int64).to(device=self.device)
+            }
+
+            rgb_fake_mask = self.get_face_parsing_mask(face_image, yv5_faces) / 255
+            # face_image = to_tensor(cv2.cvtColor(target_img, cv2.COLOR_BGR2RGB)).to(device=self.device)
+            # target_mask = self.get_face_parsing_mask(face_image, yv5_faces) / 255
+            img_mask = rgb_fake_mask * img_mask
+            fake_merged = img_mask * bgr_fake + (1-img_mask) * target_img.astype(np.float32)
+            fake_merged = fake_merged.astype(np.uint8)
+            return fake_merged
+
