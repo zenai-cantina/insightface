3b592738cc6bdf99810a8a3ea080de763bf89df1
zenai-cantina <duong.na@zenai.vn>
2024-04-11 19:38:15 +0700

    UPDATE init model zoo

--- insightface/model_zoo/__init__.py
+++ insightface/model_zoo/__init__.py
@@ -1,4 +1,4 @@
-from .model_zoo import get_model
+from .model_zoo import get_model, get_default_session_options
 from .arcface_onnx import ArcFaceONNX
 from .retinaface import RetinaFace
 from .scrfd import SCRFD
